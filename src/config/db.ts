46b8d6f17239cff246fb5d55f476027a322b0c12
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-03-14 18:52:24 +0530

    feat: Establish MongoDB connection with Mongoose and configure environment variable loading.

---
+++ src/config/db.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if (!MONGODB_URI) {
+    throw new Error("Please provide MONGODB_URI in the environment variables");
+}
+
+export function connectToDB() {
+
+    mongoose.connect(MONGODB_URI)
+        .then(() => {
+            console.log("server is connected to DB")
+        })
+        .catch(err => {
+            console.log("Error connecting to DB")
+            process.exit(1)
+        })
+
+}
+
+
+
