89c87b735000900f92a0373330089d4cbc842290
SanidhyaGupta-10 <adityasgupta1857@gmail.com>
2026-03-15 18:36:48 +0530

    feat: Add email service with Nodemailer for sending emails.

---
+++ src/service/email.service.ts
@@ -0,0 +1,25 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: process.env.EMAIL_USER,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN,
+  },
+});
+
+// Verify the connection configuration
+transporter.verify((error, success) => {
+  if (error) {
+    console.error('Error connecting to email server:', error);
+  } else {
+    console.log('Email server is ready to send messages');
+  }
+});
+
+module.exports = transporter;
